3ef7485146321278bb14a30685877038ab8c7748
miroslawdermanski <miroslavdermanski@icloud.com>
2025-03-21 01:36:35 +0200

    Extract some components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
 
 function App() {
 
     return (
         <>
             <div id="box">
-                
+                <Header />
+                <main id="main-content">
+                    <Home />
+                </main>
             </div>
         </>
     )
